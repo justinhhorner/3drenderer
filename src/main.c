cfedd0e86f10daa8ec9cf9352be0e00598b17a9f
Justin Horner <justinh.horner@gmail.com>
2025-03-16 00:59:03 -0400

    Initialize SDK window and renderer

--- src/main.c
+++ src/main.c
@@ -1,8 +1,80 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool iniitalize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error iniitalizing SDL.\n");
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        0
+    );
+    if (!window) {
+        fprintf(stderr, "Error creating window");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        fprintf(stderr, "Error creating renderer");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {
+
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                is_running = false;
+            break;
+
+    }
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
 int main(void) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    printf("This is where it all begins!\n");
+    is_running = iniitalize_window();
+
+    setup();
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+    
     return 0;
 }
